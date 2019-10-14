3b3dfc1f77cb637dd1acfb994ce4b463a2e1bf97
Alexey Dudnyk <45230390+Elektronik17@users.noreply.github.com>
2019-10-14 18:45:23 +0300

    styles added

--- client/components/dummy-view.js
+++ client/components/dummy-view.js
@@ -18,37 +18,7 @@ const Dummy = (props) => {
     <div>
       <Head title="Hello" />
       <div>{JSON.stringify(props.isRequesting)} </div>
-      <div> Page {pageIndex} {props.users.length}</div>
-      <table>
-        <tr>
-          <td>Name</td>
-          <td>Email</td>
-          <td>Company</td>
-          <td>Salary</td>
-          <td>Age</td>
-          <td>Bitcoin</td>
-          <td>whatHacked</td>
-          <td>City</td>
-          <td>Phone</td>
-          <td>Ava</td>
-        </tr>
-        {
-          !props.isRequesting && props.users.map(user => (
-            <tr>
-              <td>{user.name}</td>
-              <td>{user.email}</td>
-              <td>{user.company}</td>
-              <td>{user.salary}</td>
-              <td>{user.age}</td>
-              <td>{user.bitcoin}</td>
-              <td>{user.whatHacked}</td>
-              <td>{user.city}</td>
-              <td>{user.phone}</td>
-              <td>{user.ava}</td>
-            </tr>
-          ))
-        }
-      </table>
+      <div> Page {pageIndex} {props.users.length} </div>
       <button
         type="button"
         onClick={() => setPageIndex(Math.max(0, pageIndex - 1))}
@@ -59,6 +29,36 @@ const Dummy = (props) => {
         onClick={() => setPageIndex(pageIndex + 1)}
       >Next
       </button>
+      <table>
+        <tr>
+          <td>Ava</td>
+          <td>Name</td>
+          <td>Email</td>
+          <td>Company</td>
+          <td>Salary</td>
+          <td>Age</td>
+          <td>Bitcoin</td>
+          <td>whatHacked</td>
+          <td>City</td>
+          <td>Phone</td>
+        </tr>
+        {
+          !props.isRequesting && props.users.map(user => (
+            <tr>
+              <td><img src={user.ava} alt="" /></td>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+              <td>{user.company}</td>
+              <td>{user.salary}</td>
+              <td>{user.age}</td>
+              <td>{user.bitcon}</td>
+              <td>{user.whatHacked}</td>
+              <td>{user.city}</td>
+              <td>{user.phone}</td>
+            </tr>
+          ))
+        }
+      </table>
       <img src={`/tracker/${pageIndex}.gif`} alt="tracker" />
     </div>
   )
--- server/server.js
+++ server/server.js
@@ -64,7 +64,7 @@ const getFakeUser = () => {
     whatHacked: faker.hacker.abbreviation(),
     city: faker.address.city(),
     phone: faker.phone.phoneNumber(),
-    ava: faker.image.people()
+    ava: faker.image.avatar()
   }
 }
 const imgdata = [0x47, 0x49, 0x46, 0x38, 0x39, 0x61, 0x01, 0x00, 0x01, 0x00, 0x80, 0x00, 0x00, 0xFF, 0xFF, 0xFF,
